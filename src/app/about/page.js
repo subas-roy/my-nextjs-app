7d21db167d7861f8a020a770392c18eba4d83bb1
subas-roy <subasroy46@gmail.com>
2025-04-15 05:07:43 +0600

    Basic routing concept and nested routing

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Hello from About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/dashboard/profile/settings/page.js
@@ -0,0 +1,9 @@
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1>This is profile Settings Page</h1>
+    </div>
+  );
+};
+
+export default SettingsPage;
