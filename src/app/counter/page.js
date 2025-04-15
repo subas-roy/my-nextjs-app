0f59cd2a33161c7fd0b5d82078da43a899445bba
subas-roy <subasroy46@gmail.com>
2025-04-15 08:01:32 +0600

    Converted the entire server component to client component and create a simple counter

---
+++ src/app/counter/page.js
@@ -0,0 +1,30 @@
+'use client'; // Makes the entire serrver component to client component & allows you to use react hooks and event handlers
+
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  console.log('hello world form counter');
+  return (
+    <div className="justify-center">
+      <h1 className="text-lg">Simple Counter</h1>
+      <div>
+        <button
+          className="px-4 py-2 bg-red-500"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <span className="px-4 text-2xl">{count}</span>
+        <button
+          className="px-4 py-2 bg-green-500"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 const HomePage = () => {
+  console.log('hello world');
   return (
     <div>
       <h1 className="text-4xl text-center">Hello world!</h1>
