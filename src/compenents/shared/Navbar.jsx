fd07c84b9a384a6bb33cfb13e9884a60de0919b3
subas-roy <subasroy46@gmail.com>
2025-04-18 11:41:11 +0600

    Navigate pages without page-reload converting anchor tag to Link component

--- src/compenents/shared/Navbar.jsx
+++ src/compenents/shared/Navbar.jsx
@@ -1,25 +1,26 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 text-white py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-2xl font-bold">
+        <Link href="/" className="text-2xl font-bold">
           MySite
-        </a>
+        </Link>
         <div className="space-x-6">
-          <a href="/about" className="hover:text-gra-200">
+          <Link href="/about" className="hover:text-gra-200">
             About Us
-          </a>
-          <a href="/contact" className="hover:text-gra-200">
+          </Link>
+          <Link href="/contact" className="hover:text-gra-200">
             Contact Us
-          </a>
-          <a href="/register" className="hover:text-gra-200">
+          </Link>
+          <Link href="/register" className="hover:text-gra-200">
             Register
-          </a>
-          <a href="/login" className="hover:text-gra-200">
+          </Link>
+          <Link href="/login" className="hover:text-gra-200">
             Login
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
