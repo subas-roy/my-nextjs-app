c526eb141a840c70d64dd1a4412fc30606fb2c10
subas-roy <subasroy46@gmail.com>
2025-04-18 11:50:32 +0600

    Using Link component nextjs preface the linked component under the hood

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center">DashboardPage</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   console.log('hello world');
   return (
-    <div>
+    <div className="text-center">
       <h1 className="text-2xl text-center">Hello world!</h1>
+      <Link href="/dashboard">
+        <button className="py-5">Dashboard</button>
+      </Link>
     </div>
   );
 };
