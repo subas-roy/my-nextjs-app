3d213c827bf12036db3f13e3e7857fad212062e8
subas-roy <subasroy46@gmail.com>
2025-04-19 17:46:33 +0600

    Improve SEO with Metadata

--- src/app/(withCommonLayout)/about/page.js
+++ src/app/(withCommonLayout)/about/page.js
@@ -1,5 +1,10 @@
 import styles from './about.module.css';
 
+export const metadata = {
+  title: 'About Us',
+  description: 'This is about us page',
+};
+
 const AboutPage = () => {
   return (
     <div>
