4da0acc1cefa76b2c1953198c6724ce1684bc7e4
subas-roy <subasroy46@gmail.com>
2025-04-21 01:31:42 +0600

    Explored SSG by Next-level Data Fetching

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -1,13 +1,17 @@
 import ProductCard from '@/compenents/products/ProductCard';
 
 const HomePage = async () => {
-  const res = await fetch('http://localhost:5000/products');
+  const res = await fetch('http://localhost:5000/products', {
+    cache: 'force-cache', // view content instantly without loading
+  });
   const products = await res.json();
-  console.log(products);
+  // console.log(products);
 
   return (
     <div className="text-center">
-      <h1 className="text-2xl text-center">Welcome to Next.Js Home Page</h1>
+      <h1 className="text-2xl text-center">
+        Welcome to Next.Js Home Page page
+      </h1>
 
       <div className="grid grid-cols-3 gap-8 w-[90%] mx-auto">
         {products.map((product) => (
