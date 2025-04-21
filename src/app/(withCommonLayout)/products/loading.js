0ec5754c5a364bbfd40a46bb93fca55430b16e5d
subas-roy <subasroy46@gmail.com>
2025-04-21 14:27:30 +0600

    Craeted built-in loading page for products route/page

---
+++ src/app/(withCommonLayout)/products/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center text-blue-500">Loading.....</h1>
+    </div>
+  );
+};
+
+export default LoadingPage;
