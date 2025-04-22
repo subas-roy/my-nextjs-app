a63ff2e5654cc28822780349f3ad9ca67333236c
subas-roy <subasroy46@gmail.com>
2025-04-23 04:15:10 +0600

    Handling Active Links in Next.js using usePathname

--- src/compenents/shared/Navbar.jsx
+++ src/compenents/shared/Navbar.jsx
@@ -1,27 +1,66 @@
+'use client'; // This component is a client component, which means it can use hooks and state
+import { usePathname } from 'next/navigation'; // Importing usePathname from Next.js for routing
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+  const pathname = usePathname(); // Getting the current pathname using usePathname
+  // console.log(pathname);
   return (
-    <nav className="bg-blue-600 text-white py-4 shadow-md">
+    <nav className="bg-gray-700 text-white py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold">
           MySite
         </Link>
         <div className="space-x-6">
-          <Link href="/about" className="hover:text-gra-200">
+          <Link
+            href="/about"
+            className={
+              pathname === '/about'
+                ? 'text-blue-700 underline'
+                : 'hover:underline'
+            }
+          >
             About Us
           </Link>
-          <Link href="/contact" className="hover:text-gra-200">
+          <Link
+            href="/contact"
+            className={
+              pathname === '/contact'
+                ? 'text-blue-700 underline'
+                : 'hover:underline'
+            }
+          >
             Contact Us
           </Link>
-          <Link href="/dashboard" className="hover:text-gra-200">
+          <Link
+            href="/dashboard"
+            className={
+              pathname === '/dashboard'
+                ? 'text-blue-700 underline'
+                : 'hover:underline'
+            }
+          >
             Dashboard
           </Link>
-          <Link href="/register" className="hover:text-gra-200">
+          <Link
+            href="/register"
+            className={
+              pathname === '/register'
+                ? 'text-blue-700 underline'
+                : 'hover:underline'
+            }
+          >
             Register
           </Link>
-          <Link href="/login" className="hover:text-gra-200">
+          <Link
+            href="/login"
+            className={
+              pathname === '/login'
+                ? 'text-blue-700 underline'
+                : 'hover:underline'
+            }
+          >
             Login
           </Link>
         </div>
