4f5aadd50f2e569ab11ad7a9fa7dca34fa0167bd
subas-roy <subasroy46@gmail.com>
2025-04-23 02:42:35 +0600

    Created ErrorPage for products route only, does not affect other routes

---
+++ src/app/(withCommonLayout)/products/error.js
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-center text-red-500 my-5">
+        Something went wrong!!!
+      </h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
