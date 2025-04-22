e66129199386d154cbab3fc78f3e7a5bcffc0bee
subas-roy <subasroy46@gmail.com>
2025-04-23 03:36:24 +0600

    Added reset button to re-render the server of fix backend issue, Handle global errors with built-in error page

--- src/app/(withCommonLayout)/products/error.js
+++ src/app/(withCommonLayout)/products/error.js
@@ -9,6 +9,14 @@ const ErrorPage = ({ error, reset }) => {
         Something went wrong!!!
       </h1>
       <p className="text-center text-red-500 my-5">{error.message}</p>
+      <div className="text-center">
+        <button
+          onClick={() => reset()}
+          className="bg-red-500 p-2 rounded-md hover:bg-green-700"
+        >
+          Try Again
+        </button>
+      </div>
     </div>
   );
 };
